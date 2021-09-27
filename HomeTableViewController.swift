154e55b3f75ba30d9bb0b219b3f9834120ede094
Ben Zech <72528903+TechZech@users.noreply.github.com>
2021-09-26 22:14:42 -0400

    Twitter part 2

--- HomeTableViewController.swift
+++ HomeTableViewController.swift
@@ -20,6 +20,13 @@ class HomeTableViewController: UITableViewController {
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
     
     @objc func loadTweets(){
@@ -86,6 +93,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setLike(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
--- TweetTableViewCell.swift
+++ TweetTableViewCell.swift
@@ -13,6 +13,57 @@ class TweetTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContentLabel: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    
+    var likedTweet:Bool = false
+    var tweetId:Int = -1
+        
+    @IBAction func onClickLikeButton(_ sender: Any) {
+        let toBeLiked = !likedTweet
+        if(toBeLiked){
+            TwitterAPICaller.client?.likeTweet(tweetId: tweetId, success: {
+                self.setLike(true)
+            }, failure: { (error) in
+                print("Could Not Like: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unlikeTweet(tweetId: tweetId, success: {
+                self.setLike(false)
+            }, failure: { (error) in
+                print("Could Not Unlike: \(error)")
+            })
+        }
+    }
+    
+    @IBAction func onClickRetweetButton(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error with retweeting: \(error)")
+        })
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+    
+    func setLike(_ isLikedTweet:Bool){
+        likedTweet = isLikedTweet
+        if (likedTweet) {
+            likeButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            likeButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
